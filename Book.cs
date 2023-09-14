d815f674506d7c42f0e8cb7e42ad07cfb96c9c4c
Nomthandazo Ndweni <ndweni2658@gmail.com>
2023-09-14 10:56:40 +0200

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18.Constructors
+{
+    internal class Book
+    {
+
+        public string title;
+        public string author;
+        public int pages;
+
+        //constructor
+
+        public Book(string aTitle, string aAuthor, int aPages)
+        {
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18.Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Book book1 = new Book("Harry Potter", "JW Rowling", 400);
+
+            Console.WriteLine(book1.title);
+            Console.ReadLine();
+        }
+    }
+}
